5e0c0a92ff1cc25224d8a7f1146e7c11e3c80277
高楚鈞 <f256987584@gmail.com>
2024-09-26 12:03:22 +0800

    網站Footer製作；加入使用 Ant Design 以及其 icon

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,17 +1,85 @@
 /* eslint-disable react/prop-types */
 import styled from "styled-components";
 import Container from "../common/Container";
+import Logo from "../../images/logo.png";
+
+// ant design
+import { Input } from "antd";
+const { Search } = Input;
+
+// ant design icons
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const StyledHeader = styled.header`
   background-color: #d1011c;
   width: 100vw;
-  height: 200px;
+  padding: 16px 0px;
+`;
+
+const StyledHeaderSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Navigator = styled.div`
+  a {
+    margin: 0px 4px;
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+const Toolbar = styled.div`
+  a {
+    margin: 0px 6px;
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export default function Header() {
+  function onSearch() {}
+
   return (
     <StyledHeader>
-      <Container>This is header.</Container>
+      <Container>
+        <StyledHeaderSection>
+          <Navigator>
+            <a href="#">蝦皮購物</a>
+            <a href="#">下載</a>
+            <a href="#">追蹤我們</a>
+            <a href="#">部落格</a>
+          </Navigator>
+          <Toolbar>
+            <a href="#">通知</a>
+            <a href="#">幫助中心</a>
+            <a href="#">帳號</a>
+          </Toolbar>
+        </StyledHeaderSection>
+        <StyledHeaderSection>
+          <Link to="/">
+            <img src={Logo} alt="logo" height="48" />
+          </Link>
+          <Flex>
+            <Search
+              style={{ marginRight: 8}}
+              placeholder="在商城搜尋"
+              onSearch={onSearch}
+              enterButton
+            />
+            <Link to="/cart">
+              <ShoppingCartOutlined style={{ fontSize: 28, color: "white" }} />
+            </Link>
+          </Flex>
+        </StyledHeaderSection>
+      </Container>
     </StyledHeader>
   );
 }
